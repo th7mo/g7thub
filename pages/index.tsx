c62f05da929bc67524e178d2626b3552608c03b4
th7mo <dev@th7mo.com>
2023-03-20 18:46:32 +0100

    feat: add tailwindcss to project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
-import Head from 'next/head';
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>g7thub</title>
-        <meta name="description" content="A Github clone using the Github API" />
+        <meta
+          name="description"
+          content="A Github clone using the Github API"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
-  );
+  )
 }
